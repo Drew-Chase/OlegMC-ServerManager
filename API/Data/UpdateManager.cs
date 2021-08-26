94670d5bd63eee571eb42e10dbef1cfdc38f2139
Drew Chase <dcmanproductions@gmail.com>
2021-08-26 16:13:03 -0400

    Added Updater

---
+++ API/Data/UpdateManager.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace OlegMC.REST_API.Data
+{
+    public static class UpdateManager
+    {
+        private static readonly ChaseLabs.CLLogger.Interfaces.ILog log = Global.Logger;
+        public static bool CheckForUpdates()
+        {
+            string jsonString = string.Empty;
+            using (System.Net.WebClient client = new())
+            {
+                string os = OperatingSystem.IsWindows() ? "windows/OlegMC" : "linux";
+                try
+                {
+                    jsonString = client.DownloadString($"https://dl.openboxhosting.com/byos/{os}/OlegMC.deps.json");
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            JObject json = (JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString);
+            (int remoteRelease, int remoteMajor, int remoteMinor) = ExtractVersion(json);
+            (int localRelease, int localMajor, int localMinor) = ExtractVersion((JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText(Path.Combine(Directory.GetParent(Global.Paths.ExecutingBinary).FullName, $"{new FileInfo(Global.Paths.ExecutingBinary).Name.Replace(new FileInfo(Global.Paths.ExecutingBinary).Extension, "")}.deps.json"))));
+            if (remoteMinor != localMinor || remoteMajor != localMajor || remoteRelease != localRelease)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void Update(bool force = false)
+        {
+            if (CheckForUpdates() || force)
+            {
+                Update();
+            }
+        }
+        private static void Update()
+        {
+            string os = OperatingSystem.IsWindows() ? "oleg-updater.exe" : "oleg-updater";
+            string updateExe = Path.Combine(Path.GetTempPath(), os);
+            if (File.Exists(updateExe))
+            {
+                File.Delete(updateExe);
+            }
+
+            log.Debug($"cmd /k \"{updateExe} -path='{Directory.GetParent(Global.Paths.ExecutingBinary).Parent.FullName}'\"");
+            Console.ReadLine();
+            using (System.Net.WebClient client = new())
+            {
+                client.DownloadFile($"https://dl.openboxhosting.com/updater/{os}", updateExe);
+            }
+            System.Diagnostics.ProcessStartInfo info = new();
+            if (OperatingSystem.IsWindows())
+            {
+                info = new()
+                {
+                    FileName = "cmd",
+                    Arguments = $"/k \"{updateExe} -path='{Directory.GetParent(Global.Paths.ExecutingBinary).Parent.FullName}'\""
+                };
+            }
+
+            new System.Diagnostics.Process()
+            {
+                StartInfo = info
+
+            }.Start();
+        }
+        private static (int release, int major, int minor) ExtractVersion(JObject json)
+        {
+            string version = json["targets"][".NETCoreApp,Version=v5.0"].ToString().Split(":")[0].Replace("{", "").Replace("\"", "").Replace("OlegMC/", "").Trim();
+            int release, major, minor;
+            string[] versions = version.Split('.');
+            try
+            {
+                release = int.Parse(versions[0].ToString().Replace(".", ""));
+                major = int.Parse(versions[1].ToString().Replace(".", ""));
+                minor = int.Parse(versions[2].ToString().Replace(".", ""));
+            }
+            catch (FormatException e)
+            {
+                log.Error(e);
+                return (0, 0, 0);
+            }
+            return (release, major, minor);
+        }
+    }
+}
---
+++ Updater/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace OlegMC.Updater
+{
+    internal class Program
+    {
+        static ChaseLabs.CLLogger.Interfaces.ILog log = ChaseLabs.CLLogger.LogManager.Init().SetLogDirectory(Path.Combine(Directory.CreateDirectory(Path.Combine(Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LFInteractive", "OlegMC")).FullName, "Logs", "updater")).FullName, "latest.log")).SetPattern("[%TYPE%: %DATE%]: %MESSAGE%");
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "OlegMC Updater";
+            log.Info("Updating OlegMC - Server Manager");
+            string path = string.Empty;
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string arg = args[i].ToLower();
+                    if (arg.StartsWith("-path="))
+                    {
+                        path = arg.Replace("-path=", "");
+                        log.Debug($"PATH Identified as \"{path}\"");
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    string updateTemp = Path.Combine(Path.GetTempPath(), "oleg-server-update.zip");
+                    using System.Net.WebClient client = new();
+                    string os = OperatingSystem.IsWindows() ? "windows" : OperatingSystem.IsLinux() ? "linux" : OperatingSystem.IsMacOS() ? "osx" : "unsupported";
+                    client.DownloadFileAsync(new Uri($"https://dl.openboxhosting.com/byos.php?os={os}"), updateTemp);
+                    ProgressBar bar = new();
+                    client.DownloadProgressChanged += (sender, @event) =>
+                    {
+                        bar.Report((double)@event.ProgressPercentage / 100);
+                    };
+                    client.DownloadFileCompleted += (sender, @event) =>
+                    {
+                        //Directory.Delete(path, true);
+                        System.Threading.Thread.Sleep(500);
+                        System.IO.Compression.ZipFile.ExtractToDirectory(updateTemp, path, true);
+                        System.Threading.Thread.Sleep(500);
+                        System.Diagnostics.ProcessStartInfo info = new();
+                        if (OperatingSystem.IsWindows())
+                        {
+                            info = new()
+                            {
+                                FileName = Path.Combine(path, "OlegMC.exe"),
+                                Arguments = "-firstLaunch"
+                            };
+                        }
+                        else if (OperatingSystem.IsLinux())
+                        {
+                            info = new()
+                            {
+                                FileName = "mono",
+                                Arguments = $"{Path.Combine(path, "OlegMC")} -firstLaunch"
+                            };
+                        }
+                        else { return; }
+                        new System.Diagnostics.Process()
+                        {
+                            StartInfo = info
+                        }.Start();
+                        return;
+                    };
+                }
+            }
+            else
+            {
+                log.Error("Path is Needed");
+                Console.Write("Press Enter To Exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
